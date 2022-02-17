708b96f943bded6cf5122092ea140c6a1e15cf12
Andwerpz <andwerpooni@gmail.com>
2022-02-16 18:54:57 -0600

    implicit graph works now

--- SmallProjectShowcase1/src/state/ImplicitGraph.java
+++ SmallProjectShowcase1/src/state/ImplicitGraph.java
@@ -1,12 +1,17 @@
 package state;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Point;
+import java.awt.Stroke;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseWheelEvent;
 
 import main.MainPanel;
+import util.GraphicsTools;
 import util.Vector;
 
 public class ImplicitGraph extends State{
@@ -16,7 +21,7 @@ public class ImplicitGraph extends State{
 	double maxY = MainPanel.HEIGHT / 40;
 	double minY = -MainPanel.HEIGHT / 40;
 	
-	double squareSize = 0.1;
+	double squareSize = .3;	//real space size
 	
 	boolean mousePressed = false;
 	
@@ -61,29 +66,175 @@ public class ImplicitGraph extends State{
 		int maxYSquare = (int) (maxY / squareSize) + 1;
 		int minYSquare = (int) (minY / squareSize) - 1;
 		
+		//GraphicsTools.enableAntialiasing(g);
+		Graphics2D g2 = (Graphics2D) g;
+		g2.setStroke(new BasicStroke(2, BasicStroke.JOIN_ROUND, BasicStroke.CAP_ROUND));
+		
 		//for each square on screen, sample the middle point of the square
 		for(int i = minXSquare; i <= maxXSquare; i++) {
 			for(int j = minYSquare; j <= maxYSquare; j++) {
 				//real space coords for 
-				double xReal = i * squareSize;
-				double yReal = j * squareSize;
+				double x1Real = i * squareSize;
+				double y1Real = j * squareSize;
+				
+				double x2Real = (i + 1) * squareSize;
+				double y2Real = (j + 1) * squareSize;
 				
 				//convert real square coordinates to screen space
 				double xDiff = maxX - minX;
 				double yDiff = maxY - minY;
 				
 				//remember to invert y screen space coords
-				double xScreen = ((xReal - minX) / xDiff) * (double) MainPanel.WIDTH;
-				double yScreen = (double) MainPanel.HEIGHT - (((yReal - minY) / yDiff) * (double) MainPanel.HEIGHT);
+				double x1Screen = ((x1Real - minX) / xDiff) * (double) MainPanel.WIDTH;
+				double y1Screen = (double) MainPanel.HEIGHT - (((y1Real - minY) / yDiff) * (double) MainPanel.HEIGHT);
+				
+				double x2Screen = ((x2Real - minX) / xDiff) * (double) MainPanel.WIDTH;
+				double y2Screen = (double) MainPanel.HEIGHT - (((y2Real - minY) / yDiff) * (double) MainPanel.HEIGHT);
+				
+				x1Screen = (int) x1Screen;
+				y1Screen = (int) y1Screen;
+				
+				x2Screen = (int) x2Screen;
+				y2Screen = (int) y2Screen;
 				
 				//sample function
-				double val = yReal * Math.sin(xReal) + xReal * Math.cos(yReal);
+				double val = y1Real * Math.sin(x1Real) + x1Real * Math.cos(y1Real);
+				
+				boolean[] code = new boolean[4];
+				code[0] = testFunc(y1Real, x1Real);
+				code[1] = testFunc(y1Real, x2Real);
+				code[2] = testFunc(y2Real, x2Real);
+				code[3] = testFunc(y2Real, x1Real);
+				
+				drawSquare(g, code, (int) x1Screen, (int) y1Screen, (int) x2Screen, (int) y2Screen);
+				
+				
+				//g2.setComposite(GraphicsTools.makeComposite(0.5));
 				
 				if(val < 1) {
-					g.fillRect((int) xScreen, (int) yScreen, (int) screenXSquareSize + 1, (int) screenYSquareSize + 1);
+					
+					//g.fillRect((int) x1Screen, (int) y1Screen, (int) (Math.abs(x1Screen - x2Screen)), (int) (Math.abs(y1Screen - y2Screen)));
 				}
 				
 			}
+		}
+	}
+	
+	public boolean testFunc(double x, double y) {
+		double val = (y * Math.tan(x) + x * Math.sin(y));
+		return val < 1;
+	}
+	
+	//marching squares
+	//top left and bottom right corner
+	//code:
+	//0 1
+	//3 2
+	public void drawSquare(Graphics g, boolean[] code, int x1, int y1, int x2, int y2) {
+		
+		g.setColor(Color.BLACK);
+		Graphics2D g2 = (Graphics2D) g;
+		g2.setComposite(GraphicsTools.makeComposite(1));
+		
+		int[][] poly = new int[0][0];	//x coords, y coords
+		
+		if(code[0] && code[1] && code[2] && code[3]) {
+			//System.out.println("YES");
+			poly = new int[2][];
+			poly[0] = new int[] {x1, x2, x2, x1};
+			poly[1] = new int[] {y1, y1, y2, y2};
+		}
+		else if(!code[0] && code[1] && code[2] && code[3]) {
+			poly = new int[2][];
+			poly[0] = new int[] {x1, (x1 + x2) / 2, x2, x2, x1};
+			poly[1] = new int[] {(y1 + y2) / 2, y1, y1, y2, y2};
+			
+			g.drawLine(x1, (y1 + y2) / 2, (x1 + x2) / 2, y1);
+		}
+		else if(code[0] && !code[1] && code[2] && code[3]) {
+			poly = new int[2][];
+			poly[0] = new int[] {x1, (x1 + x2) / 2, x2, x2, x1};
+			poly[1] = new int[] {y1, y1, (y1 + y2) / 2, y2, y2};
+			
+			g.drawLine((x1 + x2) / 2, y1, x2, (y1 + y2) / 2);
+		}
+		else if(code[0] && code[1] && !code[2] && code[3]) {
+			poly = new int[2][];
+			poly[0] = new int[] {x1, x2, x2, (x1 + x2) / 2, x1};
+			poly[1] = new int[] {y1, y1, (y1 + y2) / 2, y2, y2};
+			
+			g.drawLine(x2, (y1 + y2) / 2, (x1 + x2) / 2, y2);
+		}
+		else if(code[0] && code[1] && code[2] && !code[3]) {
+			poly = new int[2][];
+			poly[0] = new int[] {x1, x2, x2, (x1 + x2) / 2, x1};
+			poly[1] = new int[] {y1, y1, y2, y2, (y1 + y2) / 2};
+			
+			g.drawLine(x1, (y1 + y2) / 2, (x1 + x2) / 2, y2);
+		}
+		else if(!code[0] && !code[1] && code[2] && code[3]) {
+			poly = new int[2][];
+			poly[0] = new int[] {x1, x2, x2, x1};
+			poly[1] = new int[] {(y1 + y2) / 2, (y1 + y2) / 2, y2, y2};
+			
+			g.drawLine(x1, (y1 + y2) / 2, x2, (y1 + y2) / 2);
+		}
+		else if(code[0] && !code[1] && !code[2] && code[3]) {
+			poly = new int[2][];
+			poly[0] = new int[] {x1, (x1 + x2) / 2, (x1 + x2) / 2, x1};
+			poly[1] = new int[] {y1, y1, y2, y2};
+			
+			g.drawLine((x1 + x2) / 2, y1, (x1 + x2) / 2, y2);
+		}
+		else if(code[0] && code[1] && !code[2] && !code[3]) {
+			poly = new int[2][];
+			poly[0] = new int[] {x1, x2, x2, x1};
+			poly[1] = new int[] {y1, y1, (y1 + y2) / 2, (y1 + y2) / 2};
+			
+			g.drawLine(x1, (y1 + y2) / 2, x2, (y1 + y2) / 2);
+		}
+		else if(!code[0] && code[1] && code[2] && !code[3]) {
+			poly = new int[2][];
+			poly[0] = new int[] {(x1 + x2) / 2, x2, x2, (x1 + x2) / 2};
+			poly[1] = new int[] {y1, y1, y2, y2};
+			
+			g.drawLine((x1 + x2) / 2, y1, (x1 + x2) / 2, y2);
+		}
+		else if(!code[0] && !code[1] && !code[2] && code[3]) {
+			poly = new int[2][];
+			poly[0] = new int[] {x1, (x1 + x2) / 2, x1};
+			poly[1] = new int[] {(y1 + y2) / 2, y2, y2};
+			
+			g.drawLine(x1, (y1 + y2) / 2, (x1 + x2) / 2, y2);
+			//g.drawLine(x2, (y1 + y2) / 2, (x1 + x2) / 2, y2);
+		}
+		else if(code[0] && !code[1] && !code[2] && !code[3]) {
+			poly = new int[2][];
+			poly[0] = new int[] {x1, (x1 + x2) / 2, x1};
+			poly[1] = new int[] {y1, y1, (y1 + y2) / 2};
+			
+			g.drawLine(x1, (y1 + y2) / 2, (x1 + x2) / 2, y1);
+		}
+		else if(!code[0] && code[1] && !code[2] && !code[3]) {
+			poly = new int[2][];
+			poly[0] = new int[] {(x1 + x2) / 2, x2, x2};
+			poly[1] = new int[] {y1, y1, (y1 + y2) / 2};
+			
+			g.drawLine((x1 + x2) / 2, y1, x2, (y1 + y2) / 2);
+		}
+		else if(!code[0] && !code[1] && code[2] && !code[3]) {
+			poly = new int[2][];
+			poly[0] = new int[] {(x1 + x2) / 2, x2, x2};
+			poly[1] = new int[] {y2, (y1 + y2) / 2, y2};
+			
+			g.drawLine(x2, (y1 + y2) / 2, (x1 + x2) / 2, y2);
+		}
+		//System.out.println(y1 + " " + y2);
+		if(poly.length != 0) {
+			g2.setComposite(GraphicsTools.makeComposite(0.1));
+			//System.out.println(x1 + " " + x2);
+			//g.setColor(Color.BLACK);
+			g.fillPolygon(poly[0], poly[1], poly[0].length);
 		}
 	}
 
